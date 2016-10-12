321a4a99c0f3e72112ca6a4b09dea2a8426b3fc5
yassinamer <yassin.amer@live.com>
2016-10-12 06:32:50 +0200

    here is my commit

--- src/program.java
+++ src/program.java
@@ -3,7 +3,7 @@ public class program {
 
 	public static void main(String[] args) {
 		Patient testpat = new Patient("Yassin", "Khalifa");
-		testpat.setSSN("11111111");
+		testpat.setSSN("111111ddd11");
 		testpat.setPhNum("12234455666");
 		DAOImpl newtest = new DAOImpl();
 		newtest.insert_patient(testpat);
